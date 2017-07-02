61505163c5c40eff6a5455a1c77a75e893240716
Chris Oloff <christian.oloff@gmail.com>
2017-07-02 21:56:06 +0200

    manually test sh script, updated documentation

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # convoy-nfs-rancher
 Use a docker-provided nfs service as container storage via convoy
+
+# Usage
+
+In `coreos.units` in `/var/lib/coreos-install/user_data`:
+
+```
+  - name: convoy-nfs.service
+    command: start
+    content: |
+      [Unit]
+      Description=Convoy NFS Daemon
+      [Service]
+      User=root
+      WorkingDirectory=/tmp
+      ExecStartPre=/usr/bin/mkdir -p /opt/bin
+      ExecStartPre=/usr/bin/curl -L -o /opt/bin/convoy-nfs-service.sh https://github.com/Uber5/convoy-nfs-rancher/releases/download/v0.1.0/convoy-nfs-service.sh
+      ExecStartPre=/usr/bin/chmod 755 /opt/bin/convoy-nfs-service.sh
+      ExecStart=/opt/bin/convoy-nfs-service.sh "mount -t nfs -o rw 10.42.202.184:/gv1 /opt/convoy" /opt/convoy
+      [Install]
+      WantedBy=multi-user.target
+```
--- convoy-nfs-service.sh
+++ convoy-nfs-service.sh
@@ -1,3 +1,26 @@
-#!/bin/bash -ex
+#!/bin/bash -x
 
-echo "experimental, noop"
+# create mount dir
+mkdir -p $2 || { echo "unable to create mount dir ($2), exiting" ; exit 1 ; }
+
+# install convoy
+curl -L -o /tmp/convoy.tar.gz https://github.com/rancher/convoy/releases/download/v0.5.0/convoy.tar.gz
+tar -C /tmp -xvzf /tmp/convoy.tar.gz
+mkdir -p /opt/bin
+cp /tmp/convoy/convoy /tmp/convoy/convoy-pdata_tools /opt/bin/
+mkdir -p /etc/docker/plugins/
+echo "unix:///var/run/convoy/convoy.sock" > /etc/docker/plugins/convoy.spec
+
+# mount and run convoy
+while :
+do
+  sleep 2
+  if mountpoint $2 ; then
+    echo "$2 mounted"
+    /opt/bin/convoy daemon --drivers vfs --driver-opts vfs.path=$2
+    echo "convoy exited, retrying"
+  else
+    echo "trying to mount: $1"
+    $1 || echo "mounting failed, cmd: $1"
+  fi
+done
